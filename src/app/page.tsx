4c5eb5a5dbd09dd2892f57529aefd23e4bb1fd3b
Bermuda Hub <raunakjaiswal2020@gmail.com>
2025-12-12 15:08:11 +0000

    Fix merge conflicts in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import AppContainer from '@/components/app-container';
 
 export default function Home() {
   return <AppContainer />;
-=======
-export default function Home() {
-  return <></>;
->>>>>>> ff639c6 (copyrights by raunakxxz)
 }
