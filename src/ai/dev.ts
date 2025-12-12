ce8b3edf644fbd9a9e2746845d955d656da4bf75
Raunakx <raunakxz69@gmail.com>
2025-12-12 21:08:59 +0545

    Update dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/suggest-items-based-on-habits.ts';
-=======
-// Flows will be imported for their side effects in this file.
->>>>>>> ff639c6 (copyrights by raunakxxz)
